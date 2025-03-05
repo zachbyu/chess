6f7f1be799d34a27d0713d74877365d19fa785bf
Zach <zcs2002@byu.edu>
2025-03-04 17:53:17 -0700

    Started User Tests

---
+++ server/src/test/java/service/ServiceUnitTests.java
@@ -0,0 +1,70 @@
+package service;
+import dataaccess.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import server.*;
+import server.handlers.LoginRequest;
+import server.handlers.LoginResult;
+import server.handlers.RegisterRequest;
+import server.handlers.RegisterResult;
+import service.*;
+import org.junit.jupiter.api.Test;
+
+public class ServiceUnitTests {
+    MemoryUserDAO userDataAccess = new MemoryUserDAO();
+    MemoryAuthDAO authDataAccess = new MemoryAuthDAO();
+    MemoryGameDAO gameDataAccess = new MemoryGameDAO();
+    UserService userService = new UserService(userDataAccess,authDataAccess);
+    AuthService authService = new AuthService(userDataAccess,authDataAccess);
+    GameService gameService = new GameService(userDataAccess,authDataAccess,gameDataAccess);
+
+    @BeforeEach
+    void setup()throws DataAccessException{
+        gameService.clearGames();
+        userService.clear();
+        authService.clear();
+    }
+
+    @Test
+    void registerTestSuccess() throws DataAccessException {
+        RegisterRequest newUser = new RegisterRequest("username", "password", "email@domain");
+        RegisterResult result = userService.register(newUser);
+
+        Assertions.assertEquals(newUser.username(), result.username());
+        Assertions.assertNotNull(result.authToken());
+    }
+
+    @Test
+    void registerTestFail() throws DataAccessException{
+        RegisterRequest newUser = new RegisterRequest("username", null, "email@domain");
+        DataAccessException ex = Assertions.assertThrows(DataAccessException.class, ()-> userService.register(newUser));
+
+        Assertions.assertEquals("Error: bad request", ex.getMessage());
+    }
+
+    @Test
+    void LoginTestSuccess() throws DataAccessException{
+        userService.register(new RegisterRequest("username", "password", "email@domain"));
+        LoginRequest user = new LoginRequest("username", "password");
+        LoginResult result = userService.login(user);
+
+        Assertions.assertEquals(user.username(), result.username());
+        Assertions.assertNotNull(result.authToken());
+    }
+
+    @Test
+    void LoginTestFail() throws DataAccessException{
+        LoginRequest user = new LoginRequest("username", "password");
+        DataAccessException ex = Assertions.assertThrows(DataAccessException.class, ()-> userService.login(user));
+
+        Assertions.assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void LogoutTestSuccess() throws DataAccessException{
+        userService.register(new RegisterRequest("username", "password", "email@domain"));
+        userService.login(new LoginRequest("username", "password"));
+
+    }
+}
