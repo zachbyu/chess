b7771451d3a66d0478d109cbe2d5a39ed1356611
Zach <zcs2002@byu.edu>
2025-04-14 16:29:15 -0600

    WebSocketHandler connect should work and send the appropriate message

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -7,6 +7,7 @@ import dataaccess.SQLAuthDAO;
 import dataaccess.SQLGameDAO;
 import dataaccess.SQLUserDAO;
 import model.AuthData;
+import model.GameData;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
@@ -50,7 +51,31 @@ public class WebSocketHandler {
     }
 
     private void connect(Session session, String username, UserGameCommand command) throws IOException, DataAccessException{
-        return;
+        System.out.println("connect command received from client");
+        int gameID = command.getGameID();
+        GameData gameData = getValidGameData(session, gameID);
+        if (gameData == null) {return;}
+        ChessGame game = gameData.game();
+        //send out messages when connected
+        connections.add(command.getGameID(), username, session);
+        //who is joining?
+        String playerType;
+        if(gameData.whiteUsername()!=null && gameData.whiteUsername().equals(username)){
+            playerType = "white";
+        } else if(gameData.blackUsername()!=null && gameData.blackUsername().equals(username)){
+            playerType = "black";
+        } else{
+            playerType = "observer";
+        }
+        //actually send message
+        String message = username + " joined game " + gameID + " as " + playerType;
+        NotificationMessage connectMessage = new NotificationMessage(NOTIFICATION, message);
+        connections.broadcast(gameID, username, connectMessage);
+        //load message back to root client
+        LoadGameMessage gameMessage = new LoadGameMessage(LOAD_GAME, game, playerType);
+        connections.sendMessage(session, gameMessage);
+        System.out.println("just sent message");
+
     }
 
     private void makeMove(Session session, String username, String message)throws IOException, DataAccessException, InvalidMoveException{
@@ -63,5 +88,14 @@ public class WebSocketHandler {
 
     private void resign(Session session, String username, UserGameCommand command) throws  DataAccessException, IOException{
         return;
+    }
+
+    private GameData getValidGameData(Session session, int gameID) throws DataAccessException, IOException {
+        GameData gameData = gameDAO.getGame(gameID);
+        if (gameData == null) {
+            connections.sendMessage(session, new ErrorMessage(ERROR, "Error: enter a valid game ID"));
+            return null;
+        }
+        return gameData;
     }
 }
